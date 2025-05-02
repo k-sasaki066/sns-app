b316eaa2ab106f1e95389dcef9e33c768a2848ed
k-sasaki <k.sasaki.bluesky@gmail.com>
2025-05-02 10:28:50 +0900

    トップ画面&コメント画面作成

--- sns-app-nuxt/components/SideNav.vue
+++ sns-app-nuxt/components/SideNav.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    
-  </div>
+    <div class="side-nav__container flex">
+        <div class="side-nav__img-wrap">
+            <img class="side-nav__img" src="/icons/logo.png" alt="share">
+        </div>
+
+        <nav class="side-var-nav__menu">
+            <div class="side-nav__list flex white">
+                <div class="side-nav__home-img-wrap">
+                    <img class="side-nav__img" src="/icons/home.png" alt="home">
+                </div>
+                <NuxtLink class="side-var-nav__link" to ="/">
+                    ホーム
+                </NuxtLink>
+            </div>
+            <form class="side-nav__logout-form flex" onsubmit="return confirm('ログアウトしますか？');">
+                <div class="side-nav__logout-img-wrap">
+                    <img class="side-nav__img" src="/icons/logout.png" alt="home">
+                </div>
+                <button class="side-nav__logout-button white" type="submit">
+                    ログアウト
+                </button>
+            </form>
+        </nav>
+
+        <form class="post-form flex white">
+            <h2 class="post-form__ttl">シェア</h2>
+            <div class="post-form__input-group">
+                <textarea class="post-form__textarea white" name="text" rows="8"></textarea>
+                <div class="error-message">
+                    message
+                </div>
+            </div>
+            <button class="form-btn white post-form-btn" type="submit">シェアする</button>
+        </form>
+    </div>
 </template>
+
+<script setup>
+import '~/assets/css/side_nav.css'
+</script>
---
+++ sns-app-nuxt/pages/posts/[id].vue
@@ -0,0 +1,32 @@
+<template>
+    <main class="main grid">
+        <SideNav></SideNav>
+        <div class="home-container white">
+            <h2 class="home-ttl">コメント</h2>
+            <Message></Message>
+
+            <div class="comment-wrap">
+                <h3 class="comment--ttl">コメント</h3>
+                <div class="comment-group flex">
+                    <p class="comment-name">username</p>
+                    <p class="comment-text">texttext</p>
+                </div>
+            </div>
+
+            <form class="comment-form flex white">
+                <div class="comment-form__input-group">
+                    <textarea class="comment-form__textarea white" name="comment" rows="1"></textarea>
+                    <div class="error-message">
+                        message
+                    </div>
+                </div>
+                <button class="form-btn white comment-form-btn" type="submit">コメント</button>
+            </form>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import '~/assets/css/auth_form.css'
+import '~/assets/css/message_detail.css'
+</script>
